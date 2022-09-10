bb33be98008b67cd63bd25e7573b01bdc3058cb6
dpound <dpound@naver.com>
2022-09-10 18:40:34 +0900

    최종 프로젝트의 apigateway입니다.

---
+++ src/main/java/com/example/auctionapigateway/AuctionApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auctionapigateway/AuctionApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
