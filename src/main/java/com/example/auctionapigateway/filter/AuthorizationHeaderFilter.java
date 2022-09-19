7c3d177a8751dad7ae925f5a93ddae472b5447f9
dpound <dpound@naver.com>
2022-09-19 15:59:19 +0900

    깃 이그노어 yml 파일 추가 깃 허브에는 올라가지 않을 예정

--- src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
@@ -1,10 +1,24 @@
 package com.example.auctionapigateway.filter;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
-@Component
 @Log4j2
-public class AuthorizationHeaderFilter {
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter> {
 
+    public AuthorizationHeaderFilter(){
+
+    }
+
+    public static class Config{
+
+    }
+
+    @Override
+    public GatewayFilter apply(AuthorizationHeaderFilter config) {
+        return null;
+    }
 }
