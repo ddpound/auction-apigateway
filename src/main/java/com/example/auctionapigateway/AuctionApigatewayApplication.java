97cfb18d7d9152d38d10b5083bfde19a77e3078f
dpound <dpound@naver.com>
2022-09-27 20:42:39 +0900

    api gateway에 현재 jwt 작업중 또한 gradle이나 yml에 공통으로 config 통일하는 과정을 거침

--- src/main/java/com/example/auctionapigateway/AuctionApigatewayApplication.java
+++ src/main/java/com/example/auctionapigateway/AuctionApigatewayApplication.java
@@ -3,7 +3,7 @@ package com.example.auctionapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.auctionapigateway","com.example.modulecommon"})
 public class AuctionApigatewayApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
@@ -1,9 +1,17 @@
 package com.example.auctionapigateway.filter;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.HttpHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Log4j2
 @Component
@@ -19,6 +27,43 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
     @Override
     public GatewayFilter apply(AuthorizationHeaderFilter config) {
-        return null;
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange,"no authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String autorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            String jwtHeader = autorizationHeader.replace("Bearer","");
+
+            // 작동 테스트 바람
+            String reFreshJwtHeader = request.getHeaders().get("RefreshToken").get(0);
+
+            if(jwtHeader == null || !jwtHeader.startsWith("Bearer")
+                    || reFreshJwtHeader == null || !reFreshJwtHeader.startsWith("Bearer")){
+                log.info("This request have not token");
+            }else{
+
+            }
+
+            log.info("API GATEWAY JWTCheckFilter has been activated.");
+
+
+
+
+
+            return chain.filter(exchange);
+        });
+    }
+
+    // Mono, Flux -> Spring WebFlux 라는 것
+    private Mono<Void> onError(ServerWebExchange exchange, String errMassage, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        log.error(errMassage);
+        return response.setComplete();
     }
 }
