fe09c3e02d43f7387de1697796dad0aa563530e1
dpound <dpound@naver.com>
2022-10-12 19:48:30 +0900

    cors 문제를 해결하기위해 config 설정중

--- src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
@@ -1,32 +1,53 @@
 package com.example.auctionapigateway.filter;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.auctionapigateway.jwtutil.JWTUtil;
+import com.example.modulecommon.model.UserModel;
+import com.example.modulecommon.repository.JwtSuperintendRepository;
+import com.example.modulecommon.repository.UserModelRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Log4j2
+import java.util.Map;
+
 @Component
-public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter> {
+@Log4j2
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
-    public AuthorizationHeaderFilter(){
+    private final JWTUtil jwtUtil;
 
-    }
+    private final UserModelRepository userModelRepository;
 
-    public static class Config{
+    private final JwtSuperintendRepository jwtSuperintendRepository;
 
+    public static class Config{}
+
+    @Autowired
+    public AuthorizationHeaderFilter(JWTUtil jwtUtil,
+                                     UserModelRepository userModelRepository,
+                                     JwtSuperintendRepository jwtSuperintendRepository){
+        super(Config.class);
+        this.jwtUtil = jwtUtil;
+        this.jwtSuperintendRepository = jwtSuperintendRepository;
+        this.userModelRepository = userModelRepository;
     }
 
     @Override
-    public GatewayFilter apply(AuthorizationHeaderFilter config) {
+    public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
 
@@ -35,25 +56,77 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             }
 
             String autorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-
-            String jwtHeader = autorizationHeader.replace("Bearer","");
-
             // 작동 테스트 바람
             String reFreshJwtHeader = request.getHeaders().get("RefreshToken").get(0);
 
-            if(jwtHeader == null || !jwtHeader.startsWith("Bearer")
-                    || reFreshJwtHeader == null || !reFreshJwtHeader.startsWith("Bearer")){
+
+            if(!autorizationHeader.startsWith("Bearer") || !reFreshJwtHeader.startsWith("Bearer")){
                 log.info("This request have not token");
             }else{
+                log.info("API GATEWAY JWTCheckFilter has been activated.");
 
+                String jwtHeader = autorizationHeader.replace("Bearer ","");
+                reFreshJwtHeader = reFreshJwtHeader.replace("Bearer ", "");
+
+                // 1일때 검증완료, -2 면 토큰 만료
+                // 이렇게 담아두면 다시 재 검증할 필요가 없음
+                Map<Integer, DecodedJWT> resultMapToken = jwtUtil.returnMapMyTokenVerify(jwtHeader);
+
+                String username;
+
+                // -2 즉 만료일 때
+                if(resultMapToken.containsKey(-2)){
+                    // 리프레시 토큰 검증 시작, 값 변경
+                    resultMapToken = jwtUtil.returnMapMyTokenVerify(reFreshJwtHeader);
+
+                }
+
+
+                // 즉 1 인 key값이 있는지 체크,
+                if(resultMapToken.containsKey(1)){
+
+                    // 리프레쉬 과정중에 에러발생함 찾아내기
+
+                    // 리프레시 토큰이라면 이게 있을것이다
+                    if(resultMapToken.get(1).getClaim("refresh").asString() != null){
+
+                        // 리프레시 토큰, 액세스 토큰 다 DB검색
+                        // 디비에 한쌍으로 검색, 만약 없다면 누가 탈취해서 임의로 값을 넣은걸 의심
+                        UserModel userModel = jwtSuperintendRepository.findByAccessTokenAndRefreshToken(jwtHeader,reFreshJwtHeader).getUser();
+
+                        String newAccessToken = jwtUtil.makeAuthToken(userModel);
+
+                        //리프레시까지 다시 재발급이아니라 액세스만 재발급해서 다시 DB한쌍에 저장
+                        //String newRefreshToken = loginFilterJWTUtil.makeRfreshToken(userModel);
+
+                        ServerHttpResponse response = exchange.getResponse();
+
+//                        response.addHeader(HttpHeaders.AUTHORIZATION,"Bearer "+ newAccessToken);
+//                        response.addHeader("RefreshToken","Bearer "+ reFreshJwtHeader);
+
+                        jwtSuperintendRepository
+                                .updateAcToken(
+                                        newAccessToken,
+                                        userModel
+                                );
+                        resultMapToken = jwtUtil.returnMapMyTokenVerify(newAccessToken);
+                    }
+
+                    // claim 안에 refresh가 있는지 체크해주기
+
+                    username  = resultMapToken.get(1).getClaim("username").asString();
+
+                    UserModel userModel =  userModelRepository.findByUsername(username);
+
+                }
+                // 여기서 만약 또 리프레시마저 만료라면 재 로그인 시도를 유도해야함
+                if(resultMapToken.containsKey(-2)){
+                    return onError(exchange,"API GATEWAY The token has expired", HttpStatus.UNAUTHORIZED);
+                }
             }
 
-            log.info("API GATEWAY JWTCheckFilter has been activated.");
 
-
-
-
-
+            // 문제 없이 여기까지왔다면 통과
             return chain.filter(exchange);
         });
     }
--- src/main/java/com/example/auctionapigateway/jwtutil/JWTUtil.java
+++ src/main/java/com/example/auctionapigateway/jwtutil/JWTUtil.java
@@ -2,11 +2,15 @@ package com.example.auctionapigateway.jwtutil;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.example.modulecommon.model.UserModel;
 
@@ -61,6 +65,40 @@ public class JWTUtil {
 
         // EpochSecond 에폭세컨드를 이용해 exp이름을 붙여 직접 시간을 지정해준다
         // 만료시간은 리프레쉬 토큰 시간에 맞춰서 넣는다
+    }
+
+    /**
+     * 키와 밸류 형식으로 한번에 해주기
+     *  1이면 검증완료, -2 이면 만료된 토큰, -1 이면 검증실패, 그냥 토큰이 검증실패
+     * */
+    public Map<Integer, DecodedJWT> returnMapMyTokenVerify(String token){
+        Map<Integer,DecodedJWT> returnMap = new HashMap<>();
+
+        try {
+            DecodedJWT verify = JWT.require(Algorithm.HMAC256(myKey)).build().verify(token);
+            log.info("success myToken verify");
+            returnMap.put(1, verify);
+            return returnMap;
+        }catch (TokenExpiredException e){
+            log.info("The myToken has expired"); // 토큰 유효시간이 지남
+
+            DecodedJWT decodeJWT = JWT.decode(token);
+            returnMap.put(-2, decodeJWT);
+
+            // 재발급이 필요, 리프레시 토큰이 있나 체크해야함
+            return returnMap;
+        }
+
+        catch (Exception e){
+            //e.printStackTrace();
+            DecodedJWT decodeJWT = JWT.decode(token);
+
+            log.info("myToken fail verify : " + decodeJWT);
+            // 실패시
+            returnMap.put(-1, decodeJWT);
+            return returnMap;
+
+        }
     }
 
 }
