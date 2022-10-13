d7c13ab09bfb1781dc563b684f146f2f3fc3f0c8
dpound <dpound@naver.com>
2022-10-13 21:26:22 +0900

    현재 cors문제를 해결완료함 어째서 인지 yml쪽이 먹히질 않아서 자바 클래스 파일로 config 설정을 마무리함

--- src/main/java/com/example/auctionapigateway/config/PreFlightCorsConfiguration.java
+++ src/main/java/com/example/auctionapigateway/config/PreFlightCorsConfiguration.java
@@ -14,31 +14,64 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Configuration
-public class CorsConfiguration {
-    private static final String ALLOWED_HEADERS = "authorization, refreshtoken, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String EXPOSE_HEADERS = "*, Authorization, Refreshtoken";
-    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
+public class PreFlightCorsConfiguration {
+
+    private static final String ALLOWED_HEADERS = "x-requested-with,authorization," +
+            "refreshtoken,Access-Control-Allow-Origin,Content-Type," +
+            "credential,X-AUTH-TOKEN,X-CSRF-TOKEN";
+
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+
+    private static final String ALLOWED_ORIGIN = "http://localhost:3000";
+
+    private static final String ALLOWED_CREDENTIALS = "true";
+
+    // 중요 해당 헤더가 없다면 axios를 통한 프론트에서 확인이 불가능함
+    private static final String EXPOSE_HEADERS = "*,Authorization,Refreshtoken," +
+            "authorization,refreshtoken";
+
+    private static final String MAX_AGE = "3600";
+
 
     @Bean
     public WebFilter corsFilter() {
+
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
+
             ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
+
+            if (CorsUtils.isPreFlightRequest(request)) {
+
                 ServerHttpResponse response = ctx.getResponse();
+
                 HttpHeaders headers = response.getHeaders();
+
                 headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE); //OPTION how long the results of a preflight request (that is the information contained in the Access-Control-Allow-Methods and Access-Control-Allow-Headers headers) can be cached.
-                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+
+                headers.add("Access-Control-Max-Age", MAX_AGE);
+
+                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
+
+                headers.add("Access-Control-Allow-Credentials",ALLOWED_CREDENTIALS);
+
+                // 프론트에서 해당 헤더를 확인할수잇게 허가해줌
                 headers.add("Access-Control-Expose-Headers", EXPOSE_HEADERS);
+
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);
                     return Mono.empty();
+
                 }
+
             }
+
             return chain.filter(ctx);
+
         };
+
     }
+
+
 }
