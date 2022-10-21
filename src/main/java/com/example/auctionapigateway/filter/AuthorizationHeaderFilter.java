c46bcd102c0df95172f3049ec94364d8184ace08
dpound <dpound@naver.com>
2022-10-21 17:35:31 +0900

    리프레시 토큰이나 일반 토큰이 들어왓을때 작동 로직을 조금씩 수정중,

--- src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/auctionapigateway/filter/AuthorizationHeaderFilter.java
@@ -68,59 +68,53 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
                 // 이렇게 담아두면 다시 재 검증할 필요가 없음
                 Map<Integer, DecodedJWT> resultMapToken = jwtUtil.returnMapMyTokenVerify(jwtHeader);
 
-                String username;
+                // -1 즉 토큰 검증이 실패했을 때
+                if(resultMapToken.containsKey(-1)){
+                    // 리프레시 토큰 검증 시작, 값 변경
+                    resultMapToken = jwtUtil.returnMapMyTokenVerify(reFreshJwtHeader);
+                    return onError(exchange,"[API GATEWAY] Token authentication failed.", HttpStatus.UNAUTHORIZED);
+                }
 
-                // -2 즉 만료일 때
+
+                // -2 즉 만료일 때, 리프레시 토큰을 체크하고 새로운 값을 넣어줘야함
                 if(resultMapToken.containsKey(-2)){
                     // 리프레시 토큰 검증 시작, 값 변경
                     resultMapToken = jwtUtil.returnMapMyTokenVerify(reFreshJwtHeader);
 
-                }
+                    // 리프레시 토큰값이 유효하다면
+                    if(resultMapToken.containsKey(1)){
 
+                        // 리프레시 토큰이라면 이게 있을것이다
+                        if(resultMapToken.get(1).getClaim("refresh").asString() != null){
 
-                // 즉 1 인 key값이 있는지 체크,
-                if(resultMapToken.containsKey(1)){
+                            // 리프레시 토큰, 액세스 토큰 다 DB검색
+                            // 디비에 한쌍으로 검색, 만약 없다면 누가 탈취해서 임의로 값을 넣은걸 의심
+                            String findUsername = jwtSuperintendRepository.findByAccessTokenAndRefreshToken(jwtHeader,reFreshJwtHeader).getUsername();
 
-                    // 리프레쉬 과정중에 에러발생함 찾아내기
+                            String newAccessToken = jwtUtil.makeAuthToken(findUsername,resultMapToken.get(1).getClaim("userId").asInt());
 
-                    // 리프레시 토큰이라면 이게 있을것이다
-                    if(resultMapToken.get(1).getClaim("refresh").asString() != null){
-
-                        // 리프레시 토큰, 액세스 토큰 다 DB검색
-                        // 디비에 한쌍으로 검색, 만약 없다면 누가 탈취해서 임의로 값을 넣은걸 의심
-                        String findUsername = jwtSuperintendRepository.findByAccessTokenAndRefreshToken(jwtHeader,reFreshJwtHeader).getUsername();
-
-                        String newAccessToken = jwtUtil.makeAuthToken(findUsername);
-
-                        //리프레시까지 다시 재발급이아니라 액세스만 재발급해서 다시 DB한쌍에 저장
-                        //String newRefreshToken = loginFilterJWTUtil.makeRfreshToken(userModel);
-
-                        ServerHttpResponse response = exchange.getResponse();
-
-//                        response.addHeader(HttpHeaders.AUTHORIZATION,"Bearer "+ newAccessToken);
-//                        response.addHeader("RefreshToken","Bearer "+ reFreshJwtHeader);
-
-                        jwtSuperintendRepository
-                                .updateAcToken(
-                                        newAccessToken,
-                                        findUsername
-                                );
-                        resultMapToken = jwtUtil.returnMapMyTokenVerify(newAccessToken);
+                            // 여기서 새로운 토큰을 만들어주고 새로 생성
+                            jwtSuperintendRepository
+                                    .updateAcToken(
+                                            newAccessToken,
+                                            findUsername
+                                    );
+                            resultMapToken = jwtUtil.returnMapMyTokenVerify(newAccessToken);
+                        }
                     }
-
-                    // claim 안에 refresh가 있는지 체크해주기
-
-                    username  = resultMapToken.get(1).getClaim("username").asString();
+                    // 여기서 만약 또 리프레시마저 만료라면 재 로그인 시도를 유도해야함
+                    if(resultMapToken.containsKey(-2)){
+                        return onError(exchange,"API GATEWAY The token has expired", HttpStatus.UNAUTHORIZED);
+                    }
+                }else if(resultMapToken.containsKey(1)) {
+                    //만료가 아니고 값이 잘 들어왔다면
+                    // 문제 없이 여기까지왔다면 통과
+                    log.info("success check api gateway AuthorizationHeader Filter");
+                    return chain.filter(exchange);
 
                 }
-                // 여기서 만약 또 리프레시마저 만료라면 재 로그인 시도를 유도해야함
-                if(resultMapToken.containsKey(-2)){
-                    return onError(exchange,"API GATEWAY The token has expired", HttpStatus.UNAUTHORIZED);
-                }
 
-                // 문제 없이 여기까지왔다면 통과
-                log.info("success check api gateway AuthorizationHeader Filter");
-                return chain.filter(exchange);
+                return onError(exchange,"Filter Error", HttpStatus.UNAUTHORIZED);
 
             }
 
